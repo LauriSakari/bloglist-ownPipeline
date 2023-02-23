01669927e3d7d98af06a1e737e07d7480134b531
Lauri Harjukoski <lauriharjukoski@Lauris-MacBook-Air.local>
2023-02-23 13:54:03 +0200

    pipelinefirst tests

--- server/utils/list_helper.js
+++ server/utils/list_helper.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+const _ = require('lodash')
 const Blogs = require('../models/blog')
 const User = require('../models/user')
 
@@ -40,6 +42,59 @@ const usersInDb = async () => {
   return users.map(u => u.toJSON())
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if ( blogs.length === 0 ) {
+    return null
+  }
+  const favourite = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+  const printFavourite = {
+    title: favourite.title,
+    author: favourite.author,
+    likes: favourite.likes
+  }
+  return printFavourite
+}
+
+const mostBlogs = (blogs) => {
+  const authors = blogs.map(b => b.author)
+
+  const blogsPerAuthor = _.values(_.groupBy(authors)).map(a => ({ author: a[0], blogs: a.length }))
+  const result = blogsPerAuthor.reduce((prev, current) => (prev.blogs > current.blogs) ? prev : current)
+
+  return result
+}
+
+const mostLikes = (blogs) => {
+  const result = blogs.reduce((authorsLikes, object) => {
+    authorsLikes[object.author] = authorsLikes[object.author] || []
+    authorsLikes[object.author].push(
+      object.likes
+    )
+    return authorsLikes
+  }, {})
+
+  const entries = Object.entries(result)
+
+  const mapped = entries.map(line => ([line[0], _.sum(line[1])]))
+  const mostLikes = mapped.reduce((prev, current) => (prev[1] > current[1]) ? prev : current)
+  const printResult = {
+    author: mostLikes[0],
+    likes: mostLikes[1]
+  }
+  return printResult
+}
+
+
 module.exports = {
-  initialBlogs, nonExistingId, blogsInDb, usersInDb
+  initialBlogs, nonExistingId, blogsInDb, usersInDb, totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
